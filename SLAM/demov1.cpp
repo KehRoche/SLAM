c08ba20b1401aba34fae2accfa5627b3c6c2043a
kehroche <kehroche@gmail.com>
2020-01-12 11:35:17 +0800

    pose trace visz finish

--- SLAM/demov1.cpp
+++ SLAM/demov1.cpp
@@ -2,7 +2,7 @@
 #include <boost/timer.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui/highgui.hpp>
-//#include <opencv2/viz.hpp>
+#include <opencv2/viz.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include "ownslam/config.h"
@@ -46,6 +46,18 @@ int main ( int argc, char** argv )
     runtime_parameters.sensing_mode = sl::SENSING_MODE_STANDARD; // Use STANDARD sensing mode
 
     ownslam::Camera::Ptr camera ( new ownslam::Camera );
+
+    // visualization
+    cv::viz::Viz3d vis ( "Visual Odometry" );
+    cv::viz::WCoordinateSystem world_coor ( 1.0 ), camera_coor ( 0.5 );
+    cv::Point3d cam_pos ( 0, -1.0, -1.0 ), cam_focal_point ( 0,0,0 ), cam_y_dir ( 0,1,0 );
+    cv::Affine3d cam_pose = cv::viz::makeCameraPose ( cam_pos, cam_focal_point, cam_y_dir );
+    vis.setViewerPose ( cam_pose );
+
+    world_coor.setRenderingProperty ( cv::viz::LINE_WIDTH, 2.0 );
+    camera_coor.setRenderingProperty ( cv::viz::LINE_WIDTH, 1.0 );
+    vis.showWidget ( "World", world_coor );
+    vis.showWidget ( "Camera", camera_coor );
     
     
     sl::Resolution image_size = zed.getResolution();
@@ -88,6 +100,17 @@ int main ( int argc, char** argv )
            break;
        SE3 Twc = pFrame->T_c_w_.inverse();
 
+        cv::Affine3d M (
+            cv::Affine3d::Mat3 (
+                Twc.rotation_matrix() ( 0,0 ), Twc.rotation_matrix() ( 0,1 ), Twc.rotation_matrix() ( 0,2 ),
+                Twc.rotation_matrix() ( 1,0 ), Twc.rotation_matrix() ( 1,1 ), Twc.rotation_matrix() ( 1,2 ),
+                Twc.rotation_matrix() ( 2,0 ), Twc.rotation_matrix() ( 2,1 ), Twc.rotation_matrix() ( 2,2 )
+            ),
+            cv::Affine3d::Vec3 (
+                Twc.translation() ( 0,0 ), Twc.translation() ( 1,0 ), Twc.translation() ( 2,0 )
+            )
+        );
+
        Mat img_show = color.clone();
        for ( auto& pt:vo->map_->map_points_ )
        {
@@ -98,6 +121,9 @@ int main ( int argc, char** argv )
 
        cv::imshow ( "image", img_show );
        cv::waitKey ( 1 );
+	vis.setWidgetPose ( "Camera", M );
+        vis.spinOnce ( 1, false );
+        cout<<endl;
    }
 
     return 0;
